96d63f7b59cf13d4a578183cef2ddaacbad89906
udohchuks <urichard132@gmail.com>
2023-06-07 21:22:12 +0100

    insertion in an ordered linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * insert_node - inserts node
+ * @head: addr of head
+ * @number: number
+ * Return: pointer to head
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current;
+	listint_t *ptr;
+
+	ptr = malloc(sizeof(listint_t));
+	ptr->n = number;
+	ptr->next = NULL;
+	current = *head;
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+	ptr->next = current->next;
+	current->next = ptr;
+	return (*head);
+}
