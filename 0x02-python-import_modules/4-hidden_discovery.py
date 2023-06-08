df995f20e175f7fde6615b155069aa35dec90ab1
udohchuks <urichard132@gmail.com>
2023-06-08 14:34:06 +0100

    fixed pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,8 @@ import importlib.util
 
 if __name__ == "__main__":
     if __name__ == "__main__":
-        spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
+        file_path = "hidden_4.pyc"
+        spec = importlib.util.spec_from_file_location("hidden_4", file_path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
         # Extract the names that do not start with "__"
