9288272b1d77083ffd976a37da40f55a6dabf1e6
udohchuks <urichard132@gmail.com>
2023-06-15 12:56:19 +0100

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_score = None
+    best_key = None
+
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if max_score is None or value > max_score:
+                max_score = value
+                best_key = key
+    return best_key
