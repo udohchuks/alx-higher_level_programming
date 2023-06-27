ad7a614fb5f3e68af748a4b8ea7f1a3edca263a2
udohchuks <urichard132@gmail.com>
2023-06-28 00:13:50 +0100

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Define a class Square
+"""
+
+
+class Square:
+    """
+    Represent a square
+
+    Attributes:
+      size: size
+    """
+    def __init__(self, size):
+        """
+        Init object
+
+        Args:
+          size: size
+        """
+        self.__size = size
