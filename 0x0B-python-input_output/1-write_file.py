9a2e39b525621a304f2ed9f6bdddf254c54b5899
udohchuks <urichard132@gmail.com>
2023-07-12 00:17:21 +0100

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """write to file"""
     with open(filename, "w", encoding="utf-8") as f:
         num_content = f.write(text)
         return num_content
