6aa440ce4a401449a73102c77fa8d7bba8fa7266
udohchuks <urichard132@gmail.com>
2023-07-12 00:23:47 +0100

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""appends to file """
+
+
+def append_write(filename="", text=""):
+    """appends to file"""
+    with open(filename, "r", encoding="utf-8") as file:
+        file.seek(0, 2)
+        num_char = file.write(text)
+        return num_char
