7170800497141d9a3797e24348db28000068c9c8
udohchuks <urichard132@gmail.com>
2023-07-11 13:01:42 +0100

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+    pass
+
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
