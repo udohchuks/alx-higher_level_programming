9dcbf5fde5f4abdf102a1e6b915ad686fc1ed786
udohchuks <urichard132@gmail.com>
2023-07-12 00:26:51 +0100

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@
 
 def append_write(filename="", text=""):
     """appends to file"""
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         file.seek(0, 2)
         num_char = file.write(text)
         return num_char
