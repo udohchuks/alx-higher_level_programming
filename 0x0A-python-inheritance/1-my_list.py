a63abbe84a67b8b2276a63cf851a8fcd8cd17eed
udohchuks <urichard132@gmail.com>
2023-07-11 12:18:47 +0100

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Class MyList
+
+"""
+
+
+class MyList(list):
+    """
+    MyList
+    """
+    pass
+
+    def print_sorted(self):
+        """
+        Sorted List
+        """
+
+        print(sorted(list(self)))
