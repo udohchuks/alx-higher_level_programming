09bb96229a5b34dcbc3bd779767d3e67e5917501
udohchuks <urichard132@gmail.com>
2023-07-12 01:14:44 +0100

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""sys
-load and sav
-"""
+"""load and sav"""
 import sys
-
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
