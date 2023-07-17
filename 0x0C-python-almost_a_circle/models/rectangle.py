3fd2a77ae1d7c3954a26344fadfa52a78434d4dc
udohchuks <urichard132@gmail.com>
2023-07-17 18:24:06 +0100

    display feature

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,8 @@ class Rectangle(Base):
     def area(self):
         """Area function"""
         return self.width * self.height
+
+    def display(self):
+        """Display"""
+        for _  in range(self.height):
+            print("#" * self.width)
