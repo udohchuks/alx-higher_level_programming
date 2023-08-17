27b00b5455e6ef7c6904bbdb61ded60f5d9d4b10
udohchuks <urichard132@gmail.com>
2023-08-17 13:42:59 -0700

    task15

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,6 @@
 SELECT tv_genres.name FROM tv_genres
 INNER JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id=8
+	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title ='DEXTER'
 ORDER BY tv_genres.name;
