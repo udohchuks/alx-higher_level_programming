cebc69cf40e988ede03524400188436456243632
CHUKWUEMEKA SAMUEL UDOH <123861233+udohchuks@users.noreply.github.com>
2024-06-30 09:15:47 -0700

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Definiton """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
