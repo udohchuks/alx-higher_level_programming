5d526c79acd9f581ca504cdb8a865bb7f71cf62b
CHUKWUEMEKA SAMUEL UDOH <123861233+udohchuks@users.noreply.github.com>
2024-07-02 11:52:43 -0700

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validator"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
