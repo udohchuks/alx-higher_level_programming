8bba15f68b60c1ee926fefb8b9b5e81034eb78b0
udohchuks <urichard132@gmail.com>
2024-07-05 11:39:58 +0100

    style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 const size = Math.floor(Number(args[0]));
 if (args.length === 0 || isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else if (size > 0) {
-    for (let i = 0; i < size; i++) {
-        console.log('X'.repeat(size));
-    }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
