1292be4f88bb0b2d764768e2ac8520ccf661c981
udohchuks <urichard132@gmail.com>
2024-07-06 13:02:41 +0100

    task10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print(f"Error code: {response.status_code}")
