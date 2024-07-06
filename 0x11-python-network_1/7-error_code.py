cc1b64654b1972485ef3394648d4971cabbe376e
udohchuks <urichard132@gmail.com>
2024-07-06 12:27:59 +0100

    __main__

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,11 +4,11 @@
 
 import requests
 import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
 
-url = sys.argv[1]
-response = requests.get(url)
-
-if response.status_code >= 400:
-    print(f"Error code: {response.status_code}")
-else:
-    print(response.text)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
