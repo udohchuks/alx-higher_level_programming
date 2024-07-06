4bc3dc2eedabe99e122a7c2aa72f5fb78e93ef36
udohchuks <urichard132@gmail.com>
2024-07-06 03:01:13 +0100

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+print("Body reponse:")
+print(f"\t- type: {type(body)}")
+print(f"\t- content: {body}")
+print(f"\t- utf8 content: {body.decode('utf-8')}")
