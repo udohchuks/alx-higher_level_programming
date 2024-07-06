82bdbd0870b17ea64304961ead221e37dd57edc8
udohchuks <urichard132@gmail.com>
2024-07-06 12:23:23 +0100

    task7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" sends a request to the URL and displays the body of the response."""
+
+import requests
+import sys
+
+url = sys.argv[1]
+response = requests.get(url)
+
+if response.status_code >= 400:
+    print(f"Error code: {response.status_code}")
+else:
+    print(response.text)
